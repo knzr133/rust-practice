b859936617fda6fb4abc9dc0dab1e5b56e3351f3
knzr133 <insraze@gmail.com>
2025-03-16 09:30:34 +0200

    Додав функцію для перевірки простих чисел

--- src/homework/hw08.rs
+++ src/homework/hw08.rs
@@ -1,18 +1,37 @@
-pub fn is_prime(n: u32) -> bool {
-    if n < 2 {
+// Функція для перевірки простоти числа
+fn is_prime(n: &u32) -> bool {
+    if *n < 2 {
         return false;
     }
-    for i in 2..=((n as f64).sqrt() as u32) {
-        if n % i == 0 {
+    for i in 2..=(((*n) as f64).sqrt() as u32) {
+        if *n % i == 0 {
             return false;
         }
     }
     true
 }
 
-fn main() {
-    let test_numbers = [1, 2, 3, 4, 5, 16, 17, 19, 20, 23, 29, 30];
-    for &num in &test_numbers {
-        println!("{} is prime: {}", num, is_prime(num));
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Тестові дані для перевірки
+    #[test]
+    fn test_is_prime() {
+        let test_data = [
+            (0, false),
+            (1, false),
+            (2, true),
+            (3, true),
+            (4, false),
+            (5, true),
+            (100, false),
+            (10007, true),
+        ];
+
+        // Перевірка кожного випадку
+        test_data.iter().for_each(|(n, prime)| {
+            assert_eq!(is_prime(n), *prime);
+        });
     }
 }
